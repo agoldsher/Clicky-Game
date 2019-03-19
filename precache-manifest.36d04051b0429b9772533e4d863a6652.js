b46e8600af09e953f833f1cf1d3cc6f3f3e3ba48
Blue Buffalo <anettagoldsher@gmail.com>
2019-03-18 23:03:37 -0700

    Updates

--- precache-manifest.36d04051b0429b9772533e4d863a6652.js
+++ precache-manifest.36d04051b0429b9772533e4d863a6652.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
   },
   {
-    "revision": "6a323d6cd385461ad4ee",
-    "url": "/Clicky-Game/static/js/main.94ff04ca.chunk.js"
+    "revision": "7fff229b69af562dfa94",
+    "url": "/Clicky-Game/static/js/main.1635ffec.chunk.js"
   },
   {
     "revision": "ba264cd90c4d36546d5a",
     "url": "/Clicky-Game/static/js/2.8f71a45e.chunk.js"
   },
   {
-    "revision": "6a323d6cd385461ad4ee",
+    "revision": "7fff229b69af562dfa94",
     "url": "/Clicky-Game/static/css/main.80d97318.chunk.css"
   },
   {
-    "revision": "f87497696c352ebadefc4b786fe3a9d5",
+    "revision": "e4a5880e34e0bd40b95ba77580732441",
     "url": "/Clicky-Game/index.html"
   }
 ];
